c4feb1d93e6cf16f31b5c5de27510e3852032bcd
Andrey Pereira <104091655+andreypereira08@users.noreply.github.com>
2022-05-13 20:32:32 -0300

    urna_eletronica

---
+++ urna_eletronica/Form1.Designer.cs
@@ -0,0 +1,232 @@
+﻿namespace urna_eletronica
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_1 = new System.Windows.Forms.Button();
+            this.btn_2 = new System.Windows.Forms.Button();
+            this.btn_3 = new System.Windows.Forms.Button();
+            this.btn_4 = new System.Windows.Forms.Button();
+            this.btn_5 = new System.Windows.Forms.Button();
+            this.btn_6 = new System.Windows.Forms.Button();
+            this.btn_7 = new System.Windows.Forms.Button();
+            this.btn_8 = new System.Windows.Forms.Button();
+            this.btn_9 = new System.Windows.Forms.Button();
+            this.btn_0 = new System.Windows.Forms.Button();
+            this.txt_visor = new System.Windows.Forms.TextBox();
+            this.btn_branco = new System.Windows.Forms.Button();
+            this.btn_corrige = new System.Windows.Forms.Button();
+            this.btn_confirma = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn_1
+            // 
+            this.btn_1.Location = new System.Drawing.Point(107, 160);
+            this.btn_1.Name = "btn_1";
+            this.btn_1.Size = new System.Drawing.Size(75, 32);
+            this.btn_1.TabIndex = 0;
+            this.btn_1.Text = "1";
+            this.btn_1.UseVisualStyleBackColor = true;
+            this.btn_1.Click += new System.EventHandler(this.btn_1_Click);
+            // 
+            // btn_2
+            // 
+            this.btn_2.Location = new System.Drawing.Point(216, 160);
+            this.btn_2.Name = "btn_2";
+            this.btn_2.Size = new System.Drawing.Size(75, 32);
+            this.btn_2.TabIndex = 1;
+            this.btn_2.Text = "2";
+            this.btn_2.UseVisualStyleBackColor = true;
+            this.btn_2.Click += new System.EventHandler(this.btn_2_Click);
+            // 
+            // btn_3
+            // 
+            this.btn_3.Location = new System.Drawing.Point(320, 160);
+            this.btn_3.Name = "btn_3";
+            this.btn_3.Size = new System.Drawing.Size(75, 32);
+            this.btn_3.TabIndex = 2;
+            this.btn_3.Text = "3";
+            this.btn_3.UseVisualStyleBackColor = true;
+            this.btn_3.Click += new System.EventHandler(this.btn_3_Click_1);
+            // 
+            // btn_4
+            // 
+            this.btn_4.Location = new System.Drawing.Point(107, 198);
+            this.btn_4.Name = "btn_4";
+            this.btn_4.Size = new System.Drawing.Size(75, 32);
+            this.btn_4.TabIndex = 3;
+            this.btn_4.Text = "4";
+            this.btn_4.UseVisualStyleBackColor = true;
+            this.btn_4.Click += new System.EventHandler(this.btn_4_Click_1);
+            // 
+            // btn_5
+            // 
+            this.btn_5.Location = new System.Drawing.Point(216, 198);
+            this.btn_5.Name = "btn_5";
+            this.btn_5.Size = new System.Drawing.Size(75, 32);
+            this.btn_5.TabIndex = 4;
+            this.btn_5.Text = "5";
+            this.btn_5.UseVisualStyleBackColor = true;
+            this.btn_5.Click += new System.EventHandler(this.btn_5_Click_1);
+            // 
+            // btn_6
+            // 
+            this.btn_6.Location = new System.Drawing.Point(319, 198);
+            this.btn_6.Name = "btn_6";
+            this.btn_6.Size = new System.Drawing.Size(75, 32);
+            this.btn_6.TabIndex = 5;
+            this.btn_6.Text = "6";
+            this.btn_6.UseVisualStyleBackColor = true;
+            this.btn_6.Click += new System.EventHandler(this.btn_6_Click_1);
+            // 
+            // btn_7
+            // 
+            this.btn_7.Location = new System.Drawing.Point(107, 236);
+            this.btn_7.Name = "btn_7";
+            this.btn_7.Size = new System.Drawing.Size(75, 30);
+            this.btn_7.TabIndex = 6;
+            this.btn_7.Text = "7";
+            this.btn_7.UseVisualStyleBackColor = true;
+            this.btn_7.Click += new System.EventHandler(this.btn_7_Click_1);
+            // 
+            // btn_8
+            // 
+            this.btn_8.Location = new System.Drawing.Point(216, 236);
+            this.btn_8.Name = "btn_8";
+            this.btn_8.Size = new System.Drawing.Size(75, 30);
+            this.btn_8.TabIndex = 7;
+            this.btn_8.Text = "8";
+            this.btn_8.UseVisualStyleBackColor = true;
+            this.btn_8.Click += new System.EventHandler(this.btn_8_Click_1);
+            // 
+            // btn_9
+            // 
+            this.btn_9.Location = new System.Drawing.Point(320, 236);
+            this.btn_9.Name = "btn_9";
+            this.btn_9.Size = new System.Drawing.Size(75, 30);
+            this.btn_9.TabIndex = 8;
+            this.btn_9.Text = "9";
+            this.btn_9.UseVisualStyleBackColor = true;
+            this.btn_9.Click += new System.EventHandler(this.btn_9_Click_1);
+            // 
+            // btn_0
+            // 
+            this.btn_0.Location = new System.Drawing.Point(216, 276);
+            this.btn_0.Name = "btn_0";
+            this.btn_0.Size = new System.Drawing.Size(75, 27);
+            this.btn_0.TabIndex = 9;
+            this.btn_0.Text = "0";
+            this.btn_0.UseVisualStyleBackColor = true;
+            this.btn_0.Click += new System.EventHandler(this.btn_0_Click_1);
+            // 
+            // txt_visor
+            // 
+            this.txt_visor.Location = new System.Drawing.Point(172, 66);
+            this.txt_visor.Name = "txt_visor";
+            this.txt_visor.Size = new System.Drawing.Size(186, 22);
+            this.txt_visor.TabIndex = 10;
+            // 
+            // btn_branco
+            // 
+            this.btn_branco.BackColor = System.Drawing.Color.White;
+            this.btn_branco.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btn_branco.Location = new System.Drawing.Point(79, 365);
+            this.btn_branco.Name = "btn_branco";
+            this.btn_branco.Size = new System.Drawing.Size(85, 31);
+            this.btn_branco.TabIndex = 11;
+            this.btn_branco.Text = "BRANCO";
+            this.btn_branco.UseVisualStyleBackColor = false;
+            this.btn_branco.Click += new System.EventHandler(this.btn_branco_Click_1);
+            // 
+            // btn_corrige
+            // 
+            this.btn_corrige.BackColor = System.Drawing.Color.Orange;
+            this.btn_corrige.Location = new System.Drawing.Point(197, 365);
+            this.btn_corrige.Name = "btn_corrige";
+            this.btn_corrige.Size = new System.Drawing.Size(94, 31);
+            this.btn_corrige.TabIndex = 12;
+            this.btn_corrige.Text = "CORRIGE";
+            this.btn_corrige.UseVisualStyleBackColor = false;
+            this.btn_corrige.Click += new System.EventHandler(this.btn_corrige_Click_1);
+            // 
+            // btn_confirma
+            // 
+            this.btn_confirma.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.btn_confirma.Location = new System.Drawing.Point(319, 365);
+            this.btn_confirma.Name = "btn_confirma";
+            this.btn_confirma.Size = new System.Drawing.Size(104, 31);
+            this.btn_confirma.TabIndex = 13;
+            this.btn_confirma.Text = "CONFIRMA";
+            this.btn_confirma.UseVisualStyleBackColor = false;
+            this.btn_confirma.Click += new System.EventHandler(this.btn_confirma_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(539, 450);
+            this.Controls.Add(this.btn_confirma);
+            this.Controls.Add(this.btn_corrige);
+            this.Controls.Add(this.btn_branco);
+            this.Controls.Add(this.txt_visor);
+            this.Controls.Add(this.btn_0);
+            this.Controls.Add(this.btn_9);
+            this.Controls.Add(this.btn_8);
+            this.Controls.Add(this.btn_7);
+            this.Controls.Add(this.btn_6);
+            this.Controls.Add(this.btn_5);
+            this.Controls.Add(this.btn_4);
+            this.Controls.Add(this.btn_3);
+            this.Controls.Add(this.btn_2);
+            this.Controls.Add(this.btn_1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_1;
+        private System.Windows.Forms.Button btn_2;
+        private System.Windows.Forms.Button btn_3;
+        private System.Windows.Forms.Button btn_4;
+        private System.Windows.Forms.Button btn_5;
+        private System.Windows.Forms.Button btn_6;
+        private System.Windows.Forms.Button btn_7;
+        private System.Windows.Forms.Button btn_8;
+        private System.Windows.Forms.Button btn_9;
+        private System.Windows.Forms.Button btn_0;
+        private System.Windows.Forms.TextBox txt_visor;
+        private System.Windows.Forms.Button btn_branco;
+        private System.Windows.Forms.Button btn_corrige;
+        private System.Windows.Forms.Button btn_confirma;
+    }
+}
+
---
+++ urna_eletronica/Form1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace urna_eletronica
+{
+    public partial class Form1 : Form
+    {
+        private int cont11;
+        private int cont22;
+        private int cont33;
+        private int contnulo;
+        private int cont_branco;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void btn_1_Click(object sender, EventArgs e)
+        {
+            txt_visor.Text = txt_visor.Text + "1";
+        }
+        private void btn_2_Click(object sender, EventArgs e)
+        {
+            txt_visor.Text = txt_visor.Text + "2";
+        }
+
+        private void btn_3_Click_1(object sender, EventArgs e)
+        {
+            txt_visor.Text = txt_visor.Text + "3";
+        }
+
+        private void btn_4_Click_1(object sender, EventArgs e)
+        {
+            txt_visor.Text = txt_visor.Text + "4";
+        }
+
+        private void btn_5_Click_1(object sender, EventArgs e)
+        {
+            txt_visor.Text = txt_visor.Text + "5";
+        }
+
+        private void btn_6_Click_1(object sender, EventArgs e)
+        {
+            txt_visor.Text = txt_visor.Text + "6";
+        }
+
+        private void btn_7_Click_1(object sender, EventArgs e)
+        {
+            txt_visor.Text = txt_visor.Text + "7";
+        }
+
+        private void btn_8_Click_1(object sender, EventArgs e)
+        {
+            txt_visor.Text = txt_visor.Text + "8";
+        }
+
+        private void btn_9_Click_1(object sender, EventArgs e)
+        {
+            txt_visor.Text = txt_visor.Text + "9";
+        }
+
+        private void btn_0_Click_1(object sender, EventArgs e)
+        {
+            txt_visor.Text = txt_visor.Text + "0";
+        }
+
+        private void btn_branco_Click_1(object sender, EventArgs e)
+        {
+            DialogResult resp;
+            resp = MessageBox.Show("confirma o voto em branco?", "Confirmação", MessageBoxButtons.YesNo);
+            if (resp == DialogResult.Yes)
+            {
+                cont_branco = cont_branco + 1;
+            }
+        }
+
+        private void btn_corrige_Click_1(object sender, EventArgs e)
+        {
+            txt_visor.Text = "";
+        }
+
+        private void btn_confirma_Click_1(object sender, EventArgs e)
+        {
+            DialogResult resp;
+            resp = MessageBox.Show("confirma o voto do numero " + txt_visor.Text + "?", "Confirmação", MessageBoxButtons.YesNo);
+
+            if (resp == DialogResult.Yes)
+            {
+                switch (txt_visor.Text)
+                {
+                    case "11":
+                        cont11 = cont11 + 1;
+                        break;
+
+                    case "22":
+                        cont22 = cont22 + 1;
+                        break;
+
+                    case "33":
+                        cont33 = cont33 + 1;
+                        break;
+
+                    default:
+                        contnulo = contnulo + 1;
+                        break;
+
+                }
+            }
+
+
+            if (txt_visor.Text == "144132")
+            {
+                MessageBox.Show("votos do candidato 11: " + cont11 + "\nvotos do candidato 22: " + cont22 + "\nvotos do candidato 33: " + cont33 + "\nvotos nulos: " + contnulo + "\nvoto branco: " + cont_branco);
+            }
+
+            txt_visor.Text = "";
+
+        }
+    }
+}
